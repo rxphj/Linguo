693cea895fe77a767ae5058b5b1294d605fee763
rxphj <raphaelpohl952004@gmail.com>
2025-08-26 12:08:00 +0200

    Classmodell

--- Backend/src/main/java/de/bund/idvk/backend/Model/User.java
+++ Backend/src/main/java/de/bund/idvk/backend/Model/User.java
@@ -1,4 +1,16 @@
 package de.bund.idvk.backend.Model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Table
+@Entity
+@Setter
+@Getter
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private Rolle rolle;
 }

e91c0fb18111dbdbad0fc99e0efa3380c76756ff
JuneS89 <y.peters89@googlemail.com>
2025-08-28 19:50:09 +0200

    Komponente ShowPoints angepasst, AdminBereich wird nur noch auf der AdminPage und nichtmehr auf der GamePage angezeigt

--- frontend/src/components/ShowPoints.jsx
+++ frontend/src/components/ShowPoints.jsx
@@ -1,11 +1,11 @@
-export default function ShowPoints( adminAdd ) {  
+export default function ShowPoints( {adminAdd} ) {  
 
 return(
 
   
     <aside className="point-section">
         <div className="points">Punkte</div>
-        <div className="admin-add">Admin Bereich</div>
+          {adminAdd && <div className="admin-add">Admin Bereich</div>}
     </aside>
 
 
